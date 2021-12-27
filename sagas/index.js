7aff4555c2cae41fc449b282b8303bb1a348ed53
Hwangsooneun <catalyst8849@gmail.com>
2021-12-27 16:44:04 +0900

    test

--- sagas/index.js
+++ sagas/index.js
@@ -3,9 +3,9 @@ import axios from "axios";
 import user from "./user";
 
 //서버 api 주소 설정 부분입니다. 사용 중인 주소로 수정하시면 됩니다.
-axios.defaults.baseURL = "http://15.165.28.19:4000/api";
+axios.defaults.baseURL = "http://15.165.28.19:80";
 axios.defaults.withCredentials = true;
 
 export default function* rootSaga() {
   yield all([call(user)]);
-}
+};
